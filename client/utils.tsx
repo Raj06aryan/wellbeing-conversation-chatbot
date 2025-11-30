e3c3fd1f3547de0ddcfdb2876617ce5edf461ce9
Raj Aryan <rajx06aryan@kgpian.iitkgp.ac.in>
2025-11-30 11:30:24 +0530

    Fix server URL variable in utils

--- client/utils.tsx
+++ client/utils.tsx
@@ -1,2 +1,3 @@
 // check if dev or prod
-export const server = process.env.NODE_ENV === "development" ? "http://localhost:8000" : process.env.NEXT_PUBLIC_SERVER_URL;
+export const server =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
